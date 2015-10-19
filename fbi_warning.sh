17660f7bd3483ac6a068a872c4747f8259252fa1
Yuchen Wu <yuchenw@cs.cmu.edu>
2015-10-19 14:36:57 -0400

    Fix idents

--- fbi_warning.sh
+++ fbi_warning.sh
@@ -1,8 +1,8 @@
-echo "$(tput cuf 10) $(tput setab 1)FBI WARNING$(tput sgr 0)"
+echo "$(tput cuf 21) $(tput setab 1) FBI WARNING $(tput sgr 0)"
 echo ""
-echo "Federal Law provides severe civil and criminal penalties for
-the unauthorized reproduction, distribution, or exhibition of
-copyrighted motion pictures (Title 17, United States Code,
-Sections 501 and 508). The Federal Bureau of Investigation
-investigates allegations of criminal copyright infringement"
-echo "$(tput cuf 5) (Title 17, United States Code, Section 506)."
+echo "$(tput cuf 0)Federal Law provides severe civil and criminal penalties for $(tput sgr 0)
+$(tput cuf 0)the unauthorized reproduction, distribution, or exhibition of $(tput sgr 0)
+$(tput cuf 0) copyrighted motion pictures (Title 17, United States Code,$(tput sgr 0)
+$(tput cuf 0)Sections 501 and 508).  The Federal Bureau of Investigation$(tput sgr 0)
+$(tput cuf 0) investigates allegations of criminal copyright infringement$(tput sgr 0)"
+echo "$(tput cuf 7) (Title 17, United States Code, Section 506)."
