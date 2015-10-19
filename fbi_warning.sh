0cfb05df4e22bd171c0da7f5c623ef6e39515ea6
Yuchen Wu <yuchenw@cs.cmu.edu>
2015-10-19 14:48:14 -0400

    Show warning only if connected via ssh

--- fbi_warning.sh
+++ fbi_warning.sh
@@ -1,8 +1,10 @@
-echo "$(tput cuf 21) $(tput setab 1) FBI WARNING $(tput sgr 0)"
-echo "$(tput cuf 0)"
-echo "$(tput setab 0)$(tput cuf 0)Federal Law provides severe civil and criminal penalties for  $(tput sgr 0)"
-echo "$(tput setab 0)$(tput cuf 0)the unauthorized reproduction, distribution, or exhibition of $(tput sgr 0)"
-echo "$(tput setab 0)$(tput cuf 0) copyrighted motion pictures (Title 17, United States Code,   $(tput sgr 0)"
-echo "$(tput setab 0)$(tput cuf 0)Sections 501 and 508).  The Federal Bureau of Investigation   $(tput sgr 0)"
-echo "$(tput setab 0)$(tput cuf 0) investigates allegations of criminal copyright infringement  $(tput sgr 0)"
-echo "$(tput setab 0)$(tput cuf 0)       (Title 17, United States Code, Section 506).           $(tput sgr 0)"
+if [ -n "$SSH_CONNECTION" ]; then
+    echo "$(tput cuf 21) $(tput setab 1) FBI WARNING $(tput sgr 0)"
+    echo "$(tput cuf 0)"
+    echo "$(tput setab 0)$(tput cuf 0)Federal Law provides severe civil and criminal penalties for  $(tput sgr 0)"
+    echo "$(tput setab 0)$(tput cuf 0)the unauthorized reproduction, distribution, or exhibition of $(tput sgr 0)"
+    echo "$(tput setab 0)$(tput cuf 0) copyrighted motion pictures (Title 17, United States Code,   $(tput sgr 0)"
+    echo "$(tput setab 0)$(tput cuf 0)Sections 501 and 508).  The Federal Bureau of Investigation   $(tput sgr 0)"
+    echo "$(tput setab 0)$(tput cuf 0) investigates allegations of criminal copyright infringement  $(tput sgr 0)"
+    echo "$(tput setab 0)$(tput cuf 0)       (Title 17, United States Code, Section 506).           $(tput sgr 0)"
+fi
